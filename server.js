9c43f4e8aaa293735ca55342cf4528fe24326307
Abhishek Kumar Bharti -X (abbharti - WIPRO LIMITED at Cisco) <abbharti@partnet.cisco.com>
2017-07-08 23:48:25 +0530

    Setup start script for Heroku

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 });
 
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
+var server = app.listen(port, function () {
+  //  var host = server.address().address
+  //  var port = server.address().port
 
-   console.log("listening at http://%s:%s", host, port);
+   console.log(`listening at http://%s:%s, ${port}`);
 
 });
